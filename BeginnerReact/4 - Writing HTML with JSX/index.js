452ca250f9fb25fe0a119ce8611c1f6796cd612a
Mark Grace <82466035+mgrace71190@users.noreply.github.com>
2022-02-25 07:17:13 +0000

    Add files via upload

---
+++ BeginnerReact/4 - Writing HTML with JSX/index.js
@@ -0,0 +1,6 @@
+
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+// this is what you want mounted + where
+render(<StorePicker> </StorePicker>, document.querySelector('#main'));
