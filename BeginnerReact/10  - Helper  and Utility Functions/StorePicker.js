776ecb7322743ae86705bba952b8dba7dadf67e5
Mark Grace <82466035+mgrace71190@users.noreply.github.com>
2022-03-07 08:08:52 +0000

    Add files via upload

---
+++ BeginnerReact/10  - Helper  and Utility Functions/StorePicker.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { getFunName } from "../helpers";
+class StorePicker extends React.Component {
+    render() {
+        return (
+        <form className="store-selector">
+            <h2>Please Enter a store</h2>
+            <input type="text" required placegolder="Store Name" defaultValue={getFunName()} />
+            <button type="submit">Visit Store</button>
+        </form>
+        );
+    }
+}
+
+export default StorePicker;
+
+
+//import react, make the storepicker, then export so it can be used in index.js
