d28dcaed3311d3d87820df8ce34e919958392f16
Mark Grace <82466035+mgrace71190@users.noreply.github.com>
2022-03-08 08:05:28 +0000

    Add files via upload

---
+++ BeginnerReact/11 - Event, Ref, this binding/StorePicker.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { getFunName } from "../helpers";
+
+class StorePicker extends React.Component {
+    myInput = React.createRef();
+
+    goToStore = (event) => {
+        // 1. Stop the form from submitting
+        event.preventDefault();
+        // 2. Get the text from the input
+        const storeName = this.myInput.value.value;
+        // 3. Change teh page to /store/whatever we entered
+        this.props.history.push(`/store/${storeName}`);
+    };
+    render() {
+        return (
+        <form className="store-selector" onSubmit={this.goToStore}>
+            <h2>Please Enter a store</h2>
+            <input 
+                type="text" 
+                ref={this.myInput}
+                required placegolder="Store Name" 
+                defaultValue={getFunName()} 
+                />
+            <button type="submit">Visit Store</button>
+        </form>
+        );
+    }
+}
+
+export default StorePicker;
+
+
+//import react, make the storepicker, then export so it can be used in index.js
